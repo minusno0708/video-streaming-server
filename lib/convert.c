a3a9caa7f71717eaf0074f1df415a1dc76d8f8a9
minusno0708 <naoyao1014@gmail.com>
2024-04-22 03:58:13 +0900

    feat:エンコード結果を出力するファイルを生成

--- lib/convert.c
+++ lib/convert.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
+#define SEGMENT_DURATION 10
+
 void convert_to_m3u8(char *filepath) {
     printf("convert %s\n", filepath);
+
+    FILE *base = fopen(filepath, "r");
+    FILE *encoded = fopen("../videos/sample.m3u8", "w");
+
+    if (base == NULL || encoded == NULL) {
+        printf("Failed to open file\n");
+        return;
+    }
+
+    fclose(base);
+    fclose(encoded);
 }
 
 int main() {
